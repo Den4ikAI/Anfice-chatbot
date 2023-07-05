3a91ce49872ad529b77b88ca16b3570152314b36
Denis <61190545+Den4ikAI@users.noreply.github.com>
2023-07-05 18:20:42 +0800

    Переход на модель SiberianFRED

--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
 # Конфигурационный файл системы
 
-VERSION = 4
+VERSION = 4.1
 
-instructor_path = 'Den4ikAI/FRED-T5-XL_instructor_chitchat' # Инструкционная модель
+instructor_path = 'SiberiaSoft/SiberianFRED-T5-XL' # Инструкционная модель
 phrase_classifier_path = 'Den4ikAI/ruBert-tiny-replicas-classifier' # Классификатор фраз пользователя
 question_classifier_path = 'Den4ikAI/ruBert-tiny-questions-classifier' # Классификатор вопросов
 embedding_model_path = 'sentence-transformers/distiluse-base-multilingual-cased-v1' # Эмбеддинг модель
